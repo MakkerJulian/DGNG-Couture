d5c3bea4f45c806a705f8ba103f70740c5a48f9e
MakkerJulian <juliankloosterhuis@gmail.com>
2024-05-21 16:47:25 +0200

    clean

--- front-end/src/axios/index.tsx
+++ front-end/src/axios/index.tsx
@@ -1,40 +1,10 @@
 import axios, { AxiosInstance } from 'axios';
-import { JwtPayload, jwtDecode } from 'jwt-decode';
-import { redirect } from 'react-router-dom';
+import { JwtPayload } from 'jwt-decode';
 
-
-const loginInstance: AxiosInstance = axios.create({
-    baseURL: `http://${import.meta.env.VITE_APP_URL ?? "localhost"}:3000`,
-    timeout: 5000,
-    proxy: false,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
-})
 
 export type Token = JwtPayload & {
     email: string;
     role : string;
-}
-
-const token = () => {
-    const sessiontoken = localStorage.getItem('token');
-    if (!sessiontoken) {
-        redirect('/login');
-    } else {
-        // const exp = jwtDecode(sessiontoken).exp ?? 0;
-        // const email = (jwtDecode(sessiontoken) as unknown as Token).email ?? "";
-        // const current_time = Date.now().valueOf() / 1000;
-        // if (current_time > exp) {
-        //     const pw = localStorage.getItem('pw');
-        //     loginInstance.post('/account/login', { email: email, password: pw }).then((res) => {
-        //         localStorage.setItem('token', res.data.access_token);
-        //         return res.data.access_token;
-        //     });
-        // }
-    }
-    return sessiontoken;
 }
 
 const axiosInstance: AxiosInstance = axios.create({
