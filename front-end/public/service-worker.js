4d249ea0b785adde5d62c400b12ceb8ff6cf3748
MakkerJulian <juliankloosterhuis@gmail.com>
2024-06-03 13:16:42 +0200

    serviceworker?

---
+++ front-end/public/service-worker.js
@@ -0,0 +1,71 @@
+// service-worker.js
+
+// Cache name
+const CACHE_NAME = 'vite-react-cache';
+
+// Files to cache
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/favicon.ico',
+    // Add other static assets you want to cache here
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return cached response if found
+                if (response) {
+                    return response;
+                }
+
+                // Fetch and cache new requests
+                return fetch(event.request)
+                    .then((response) => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        // Cache the response
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => {
+                return Promise.all(
+                    cacheNames.filter((name) => {
+                        return name !== CACHE_NAME;
+                    }).map((name) => {
+                        return caches.delete(name);
+                    })
+                );
+            })
+    );
+});
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { SnackbarProvider } from 'notistack';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+  });
+}
 
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <SnackbarProvider maxSnack={3}>
-    <App />
+      <App />
     </SnackbarProvider>
   </React.StrictMode>,
 )
-
-
