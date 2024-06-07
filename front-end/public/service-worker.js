b24ee9c34eb63b380234f11510c3430357ff40f4
Koninglukaas <Lucasbruins@outlook.com>
2024-06-07 13:32:49 +0200

    Serviceworker fix (commented)

--- front-end/public/service-worker.js
+++ front-end/public/service-worker.js
@@ -1,72 +1,70 @@
-// service-worker.js
+// // service-worker.js
 
-// Cache name
-const CACHE_NAME = 'vite-react-cache';
+// // Cache name
+// const CACHE_NAME = 'vite-react-cache';
 
-// Files to cache
-const urlsToCache = [
-    '/',
-    '/home',
-    '/index.html',
-    '/favicon.ico',
-    // Add other static assets you want to cache here
-];
+// // Files to cache
+// const urlsToCache = [
+//     '/',
+//     '/home'
+//     // Add other static assets you want to cache here
+// ];
 
-// Install event
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
+// // Install event
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches.open(CACHE_NAME)
+//             .then((cache) => {
+//                 return cache.addAll(urlsToCache);
+//             })
+//     );
+// });
 
-// Fetch event
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Return cached response if found
-                if (response) {
-                    return response;
-                }
+// // Fetch event
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then((response) => {
+//                 // Return cached response if found
+//                 if (response) {
+//                     return response;
+//                 }
 
-                // Fetch and cache new requests
-                return fetch(event.request)
-                    .then((response) => {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
+//                 // Fetch and cache new requests
+//                 return fetch(event.request)
+//                     .then((response) => {
+//                         // Check if we received a valid response
+//                         if (!response || response.status !== 200 || response.type !== 'basic') {
+//                             return response;
+//                         }
 
-                        // Clone the response
-                        const responseToCache = response.clone();
+//                         // Clone the response
+//                         const responseToCache = response.clone();
 
-                        // Cache the response
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                cache.put(event.request, responseToCache);
-                            });
+//                         // Cache the response
+//                         caches.open(CACHE_NAME)
+//                             .then((cache) => {
+//                                 cache.put(event.request, responseToCache);
+//                             });
 
-                        return response;
-                    });
-            })
-    );
-});
+//                         return response;
+//                     });
+//             })
+//     );
+// });
 
-// Activate event
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys()
-            .then((cacheNames) => {
-                return Promise.all(
-                    cacheNames.filter((name) => {
-                        return name !== CACHE_NAME;
-                    }).map((name) => {
-                        return caches.delete(name);
-                    })
-                );
-            })
-    );
-});
+// // Activate event
+// self.addEventListener('activate', (event) => {
+//     event.waitUntil(
+//         caches.keys()
+//             .then((cacheNames) => {
+//                 return Promise.all(
+//                     cacheNames.filter((name) => {
+//                         return name !== CACHE_NAME;
+//                     }).map((name) => {
+//                         return caches.delete(name);
+//                     })
+//                 );
+//             })
+//     );
+// });
