5c8fc6f43375bf1de2a98f0bfc93eb921537647e
Kars <cdhekman@gmail.com>
2024-06-19 15:41:01 +0200

    Small merge error fix

--- front-end/src/components/cityTab.tsx
+++ front-end/src/components/cityTab.tsx
@@ -20,28 +20,26 @@ export const CityTab = ({ country, city, feelTemp, weatherData }: CityTabProps) 
 
         <button className="cityButton" onClick={() => navigate(`/city?city=${city}&country=${country}`)}>
             <Box className="cityCard" sx={{ backgroundImage: `url(${Clouds})` }}>
-                <Box className="cityCard" sx={{ backgroundImage: `url(${Clouds})` }}>
-                    <Box className="cityCardText">
-                        <Typography>{weatherData.temp}°</Typography>
-                        {weatherData.clouds > 50 && <Icons.CloudIcon />}
-                        {weatherData.freezing && <Icons.AcUnitIcon />}
-                        {weatherData.rain && <Icons.WaterDropIcon />}
-                        {weatherData.snow && <Icons.SevereColdIcon />}
-                        {weatherData.hail && <Icons.StormIcon />}
-                        {weatherData.thunder && <Icons.ThunderstormIcon />}
-                        {weatherData.tornado && <Icons.TornadoIcon />}
-                    </Box>
+                <Box className="cityCardText">
+                    <Typography>{weatherData.temp}°</Typography>
+                    {weatherData.clouds > 50 && <Icons.CloudIcon />}
+                    {weatherData.freezing && <Icons.AcUnitIcon />}
+                    {weatherData.rain && <Icons.WaterDropIcon />}
+                    {weatherData.snow && <Icons.SevereColdIcon />}
+                    {weatherData.hail && <Icons.StormIcon />}
+                    {weatherData.thunder && <Icons.ThunderstormIcon />}
+                    {weatherData.tornado && <Icons.TornadoIcon />}
+                </Box>
 
 
-                    <Box className="cityCardName">
-                        <Typography>{city}</Typography>
-                    </Box>
+                <Box className="cityCardName">
+                    <Typography>{city}</Typography>
+                </Box>
 
-                    <Box className="cityCardTextInfo">
-                        <Typography>Feels like {feelTemp}</Typography>
-                        <Typography>Wind {weatherData.windspeed}km/h</Typography>
-                        <Typography>Precip {weatherData.precipitation}%</Typography>
-                    </Box>
+                <Box className="cityCardTextInfo">
+                    <Typography>Feels like {feelTemp}</Typography>
+                    <Typography>Wind {weatherData.windspeed}km/h</Typography>
+                    <Typography>Precip {weatherData.precipitation}%</Typography>
                 </Box>
             </Box>
         </button>
